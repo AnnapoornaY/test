992e996cc59aa1421a521d95dd3e8d50907fbc4f
AnnapoornaY <annapoorna.yaligar@slkgroup.com>
2021-07-28 17:19:29 +0530

    version

---
+++ AppServer/src/HelloWorld.java
@@ -0,0 +1,12 @@
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+
+@WebService
+	@SOAPBinding(style = Style.DOCUMENT) //optional
+	public interface HelloWorld{
+	    
+	    @WebMethod String getHelloWorldAsString(String name);
+
+}
---
+++ AppServer/src/HelloWorldImpl.java
@@ -0,0 +1,11 @@
+import javax.jws.WebService;
+
+@WebService(endpointInterface = "com.mkyong.ws.HelloWorld")
+public class HelloWorldImpl implements HelloWorld{
+
+    @Override
+    public String getHelloWorldAsString(String name) {
+        return "Hello World JAX-WS " + name;
+    }
+}
+
